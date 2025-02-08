763edc489be2160c830c9705d5229995f01f396e
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-02-08 16:53:25 +0900

    #39 [FEAT] ModalHashtag 컴포넌트 추가

---
+++ src/assets/svg/CloseCircle.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgCloseCircle = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 20 20"
+    {...props}
+  >
+    <circle cx={10.385} cy={9.728} r={9} fill="#3F4B5D" stroke="#4A6285" />
+    <path stroke="#fff" strokeLinecap="round" d="m6.885 13.228 7-7m-7 0 7 7" />
+  </svg>
+);
+export default SvgCloseCircle;
---
+++ src/components/ModalHashtag/ModalHashtag.tsx
@@ -0,0 +1,26 @@
+import { CloseCircle } from "@/assets/svg";
+import type { HashTagTypes } from "@/constants/hashTagsDummy";
+import * as s from "./ModalHashtag.styles";
+
+interface ModalHashtagProps extends HashTagTypes {
+  removeHashtag: (id: number) => void;
+}
+
+const ModalHashtag = ({ id, content, removeHashtag }: ModalHashtagProps) => {
+  return (
+    <div css={s.hashtagLayoutStyle}>
+      <button
+        type="button"
+        css={s.deleteStyle}
+        onClick={() => removeHashtag(id)}
+      >
+        <CloseCircle width={18} />
+      </button>
+      <button type="button" css={s.hashtagStyle}>
+        {content}
+      </button>
+    </div>
+  );
+};
+
+export default ModalHashtag;
