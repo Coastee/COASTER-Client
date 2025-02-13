da590cb563a97b09eb1b71326a995b5af043d397
namdaeun <nde40345@gmail.com>
2025-02-14 05:13:05 +0900

    #42 [FEAT] 채팅 상태에 따라 컴포넌트 렌더링

--- src/pages/DMPage/DMPage.tsx
+++ src/pages/DMPage/DMPage.tsx
@@ -1,12 +1,16 @@
 import DMList from "@/pages/DMPage/components/DMList/DMList";
+import EmptyPanel from "@/pages/DMPage/components/EmptyPanel/EmptyPanel";
 import ChatRoom from "@/pages/GroupChatPage/components/ChatRoom/ChatRoom";
+import { useState } from "react";
 
 const DMPage = () => {
+  const [isChatting, setIsChatting] = useState(false);
+
   return (
     <>
       <DMList />
-      <ChatRoom />
-      {/* <EmptyPanel /> */}
+      {/* 후에 로직 수정 */}
+      {isChatting ? <ChatRoom /> : <EmptyPanel onClick={() => setIsChatting(true)} />}
     </>
   );
 };
