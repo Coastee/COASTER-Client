5ffb8f8fae3ef93d1f8c80be97cd950008353e13
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-02-13 17:54:33 +0900

    #39 [FIX] dark1 theme 적용

--- src/pages/GroupChatListPage/components/AddGroupChatModal/AddGroupChatModal.styles.ts
+++ src/pages/GroupChatListPage/components/AddGroupChatModal/AddGroupChatModal.styles.ts
@@ -67,14 +67,13 @@ export const fileButtonStyle = css`
   font-weight: 400;
   color: ${theme.color.gray2};
   text-decoration-line: underline;
+  text-underline-position: from-font;
 
   border-radius: 10px;
-  border: 1px solid #4a6285;
+  border: 1px solid ${theme.color.dark1};
 
   background-color: transparent;
   background-color: #2d3643;
-
-  text-underline-position: from-font;
 
   transition: 0.3s ease-in-out;
 
