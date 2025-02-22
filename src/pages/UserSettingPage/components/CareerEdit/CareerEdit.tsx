ec0faed8b703605a188c1690f8d0ffec8d924e38
namdaeun <nde40345@gmail.com>
2025-02-22 15:53:34 +0900

    #40 [FEAT] 버튼 핸들러 추가

--- src/pages/UserSettingPage/components/CareerEdit/CareerEdit.tsx
+++ src/pages/UserSettingPage/components/CareerEdit/CareerEdit.tsx
@@ -1,25 +1,34 @@
-import { PlusIcon } from "@/assets/svg";
+import { CloseCircleIcon, PlusIcon } from "@/assets/svg";
 import { Button } from "@/components";
-import CareerBox from "@/pages/MyPage/components/CareerBox/CareerBox";
+import { deleteStyle } from "@/components/TagChip/TagChip.styles";
 import { careerData } from "@/pages/MyPage/constants/dummy";
+import CareerBoxChip from "@/pages/UserSettingPage/components/CareerBoxChip/CareerBoxChip";
 import * as s from "@/pages/UserSettingPage/components/CareerEdit/CareerEdit.styles";
+import { useNavigate } from "react-router-dom";
 
 const CareerEdit = () => {
+  const navigate = useNavigate();
+
   return (
     <div css={s.pageStyle}>
       <div css={s.fixedLayoutStyle}>
         <h1 css={s.titleStyle}>경력 편집</h1>
-        <button type="button" css={s.plusButtonStyle}>
+        <button type="button" css={s.plusButtonStyle} onClick={() => {}}>
           <PlusIcon width={20} height={20} />
         </button>
       </div>
       <ul css={s.listStyle}>
         {careerData.map((career, index) => (
-          <CareerBox key={`${index}-${career.title}`} {...career} />
+          <li key={`${index}-${career.title}`}>
+            <CareerBoxChip {...career} css={{ position: "relative" }} />
+            <button type="button" css={deleteStyle}>
+              <CloseCircleIcon width={18} />
+            </button>
+          </li>
         ))}
       </ul>
       <div css={s.buttonLayoutStyle}>
-        <Button variant="tertiary" size="medium">
+        <Button variant="tertiary" size="medium" onClick={() => navigate("/mypage")}>
           뒤로 가기
         </Button>
       </div>
