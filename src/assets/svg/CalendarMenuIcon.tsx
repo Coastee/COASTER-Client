d0c93ea7ff7816a7c0da0d88d82fa2b13e3ca43f
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-02-25 16:25:57 +0900

    #53 [FEAT] 전역 메뉴 아이콘 추가

---
+++ src/assets/svg/CalendarMenuIcon.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgCalendarMenuIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 26 30"
+    {...props}
+  >
+    <rect width={25} height={25} x={0.625} y={4.615} fill="#6B8AFD" rx={1} />
+    <rect width={25} height={6.722} x={0.625} y={4.615} fill="#B6CBFF" rx={1} />
+    <rect width={3.75} height={3.75} x={3.46} y={0.865} fill="#6B8AFD" rx={1} />
+    <rect
+      width={3.75}
+      height={3.75}
+      x={19.04}
+      y={0.865}
+      fill="#6B8AFD"
+      rx={1}
+    />
+    <rect width={3.163} height={3.163} x={4} y={15.835} fill="#B6CBFF" rx={1} />
+    <rect width={3.163} height={3.163} x={4} y={21.667} fill="#B6CBFF" rx={1} />
+    <rect
+      width={3.163}
+      height={3.163}
+      x={9.03}
+      y={15.835}
+      fill="#B6CBFF"
+      rx={1}
+    />
+    <rect
+      width={3.163}
+      height={3.163}
+      x={9.03}
+      y={21.667}
+      fill="#B6CBFF"
+      rx={1}
+    />
+    <rect
+      width={3.163}
+      height={3.163}
+      x={14.058}
+      y={15.835}
+      fill="#B6CBFF"
+      rx={1}
+    />
+    <rect
+      width={3.163}
+      height={3.163}
+      x={14.058}
+      y={21.667}
+      fill="#B6CBFF"
+      rx={1}
+    />
+    <rect
+      width={3.163}
+      height={3.163}
+      x={19.087}
+      y={15.835}
+      fill="#B6CBFF"
+      rx={1}
+    />
+    <rect
+      width={3.163}
+      height={3.163}
+      x={19.087}
+      y={21.667}
+      fill="#B6CBFF"
+      rx={1}
+    />
+  </svg>
+);
+export default SvgCalendarMenuIcon;
---
+++ src/assets/svg/DmMenuIcon.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgDmMenuIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 28 28"
+    {...props}
+  >
+    <path
+      fill="#6B8AFD"
+      d="M25.874 1.376a1 1 0 0 1 1.24 1.24L20.597 24.96c-.27.925-1.563.971-1.898.067l-3.957-10.689a1 1 0 0 0-.59-.59L3.462 9.79c-.903-.335-.857-1.628.068-1.898z"
+    />
+    <path
+      stroke="#B6CBFF"
+      strokeLinecap="square"
+      d="m15.299 12.996 5.87-5.517"
+    />
+  </svg>
+);
+export default SvgDmMenuIcon;
