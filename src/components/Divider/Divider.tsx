f5d81c7d1abe5d65feb4474edf4163aba8f1bed6
namdaeun <nde40345@gmail.com>
2025-02-25 20:24:23 +0900

    #55 [FEAT] Divider 방향 추가

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -4,14 +4,15 @@ import type { HTMLAttributes } from "react";
 
 interface DividerProps extends HTMLAttributes<HTMLHRElement> {
   variant?: "single" | "date";
+  direction?: "vertical" | "horizontal";
 }
 
-const Divider = ({ variant = "single", children, ...props }: DividerProps) => {
+const Divider = ({ variant = "single", direction = "vertical", children, ...props }: DividerProps) => {
   return (
     <div css={s.wrapperStyle(variant)} {...props}>
-      <hr css={s.dividerStyle} />
+      <hr css={s.dividerStyle(direction)} />
       {variant === "date" && <p css={s.childrenStyle}>{children}</p>}
-      <hr css={s.dividerStyle} />
+      <hr css={s.dividerStyle(direction)} />
     </div>
   );
 };
