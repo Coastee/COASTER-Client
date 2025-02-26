9bfef2531ad3b7d02c8c18deed6bfd18f7c371a7
namdaeun <nde40345@gmail.com>
2025-02-27 02:06:15 +0900

    #40 [CHORE] 필요 없는 코드 제거

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,9 +1,15 @@
 import App, { layoutStyle } from "@/App";
 import { ServerHeader } from "@/components";
-import { CoffeeChatListPage, DMPage, GroupChatListPage, HomePage, MyPage, OnboardingPage, SignupPage } from "@/pages";
+import {
+  CoffeeChatListPage,
+  DMPage,
+  GroupChatListPage,
+  HomePage,
+  MyPage,
+  OnboardingPage,
+  SignupPage,
+} from "@/pages";
 import GroupChatPage from "@/pages/GroupChatPage/GroupChatPage";
-import CareerAdd from "@/pages/UserSettingPage/components/CareerAdd/CareerAdd";
-import EditCareer from "@/pages/UserSettingPage/components/CareerEdit/EditCareer";
 import CareerSettingList from "@/pages/UserSettingPage/components/CareerSettingList/CareerSettingList";
 import ProfileEdit from "@/pages/UserSettingPage/components/ProfileEdit/ProfileEdit";
 import { Outlet, createBrowserRouter } from "react-router-dom";
@@ -57,14 +63,6 @@ export const router = createBrowserRouter([
       {
         path: "/mypage/setting-career",
         element: <CareerSettingList />,
-      },
-      {
-        path: "/mypage/setting-edit-career",
-        element: <EditCareer />,
-      },
-      {
-        path: "/mypage/setting-add-career",
-        element: <CareerAdd />,
       },
     ],
   },
