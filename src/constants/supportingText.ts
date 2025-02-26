ed2df95ed686a310fca76d131ea675a484039f3e
namdaeun <nde40345@gmail.com>
2025-02-27 03:47:49 +0900

    #40 [CHORE] maxLength 수정

--- src/constants/supportingText.ts
+++ src/constants/supportingText.ts
@@ -2,8 +2,8 @@ export const SUPPORTING_TEXT = {
   NICKNAME: "2~10자 이내로 입력해주세요",
   BIRTH: "입력 형식을 확인해주세요 (YYYY.MM.DD)",
   REQUIRED: "필수 입력 항목입니다",
-  CAREER: "20자 이내로 입력해주세요",
+  CAREER: "15자 이내로 입력해주세요",
   CAREER_YEAR: "숫자로 입력해주세요",
-  ONE_LINE_INTRO: "20자 이내로 입력해주세요",
+  ONE_LINE_INTRO: "40자 이내로 입력해주세요",
   INTRO: "60자 이내로 입력해주세요",
 };
--- src/pages/UserSettingPage/constants/maxLength.ts
+++ src/pages/UserSettingPage/constants/maxLength.ts
@@ -1,9 +1,13 @@
 export const MAX_LENGTH = {
-  NICKNAME_MIN: 2,
-  NICKNAME_MAX: 10,
-  CAREER: 20,
-  ONELINE_INTRO: 20,
+  NICKNAME: 10,
+  CAREER: 15,
+  ONELINE_INTRO: 40,
   INTRO: 60,
   DETAIL_COUNT: 5,
   DETAIL: 30,
+};
+
+export const MIN_LENGTH = {
+  NICKNAME: 2,
+  CAREER: 1,
 };
