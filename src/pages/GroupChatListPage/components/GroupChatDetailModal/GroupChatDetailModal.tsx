0171bf879dd013ef1ccb6ce37e0c655d62b0b6a1
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-03-03 04:43:31 +0900

    #63 [FEAT] 그룹챗 리스트에서 각 아이템 클릭 시 상세 모달 뜨도록(1)

--- src/pages/GroupChatListPage/components/GroupChatDetailModal/GroupChatDetailModal.tsx
+++ src/pages/GroupChatListPage/components/GroupChatDetailModal/GroupChatDetailModal.tsx
@@ -1,41 +1,55 @@
-import thumbnail from "@//assets/img/detailBgImg.png";
+import detailBgImg from "@/assets/img/detailBgImg.png";
 import { SideModal, TagChip } from "@/components";
 import type { SideModalProps } from "@/components/SideModal/types/sideModalTypes";
-import * as s from "./GroupChatDetailModal.styles";
-
 import ChatDetailUserInfo from "@/pages/GroupChatListPage/components/ChatDetailUserInfo/ChatDetailUserInfo";
 import HamburgerMenu from "@/pages/GroupChatListPage/components/HamburgerMenu/HamburgerMenu";
-import { GROUP_CHAT_DETAIL_DUMMY } from "@/pages/GroupChatListPage/constants/groupChatDetailDummy";
 import { useState } from "react";
+import * as s from "./GroupChatDetailModal.styles";
 
-const GroupChatDetailModal = ({ isVisible, setIsVisible }: SideModalProps) => {
+interface GroupChatDetailModalProps extends SideModalProps {
+  id: number;
+  title: string;
+  thumbnail?: string | null;
+  content: string;
+  hashTagList: { id: number; content: string }[];
+  nickname: string;
+  expYears: number;
+  job: string;
+}
+
+const GroupChatDetailModal = ({
+  id,
+  title,
+  thumbnail,
+  content,
+  hashTagList,
+  nickname,
+  expYears,
+  job,
+  isVisible,
+  setIsVisible,
+}: GroupChatDetailModalProps) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const detail = GROUP_CHAT_DETAIL_DUMMY;
 
   return (
     <SideModal
       modalStyle={{ width: "100%", maxWidth: "70rem" }}
-      title={detail.title}
+      title={title}
       isVisible={isVisible}
       setIsVisible={setIsVisible}
       extraButton={<HamburgerMenu isVisible={isMenuOpen} setIsVisible={setIsMenuOpen} />}
     >
       <div css={s.contentStyle}>
-        <ChatDetailUserInfo
-          nickname={detail.user.nickname}
-          expYears={detail.user.userIntro.expYears}
-          job={detail.user.userIntro.job}
-        />
-        <img src={detail.thumbnail || thumbnail} css={s.imageStyle} alt="썸네일" />
+        <ChatDetailUserInfo nickname={nickname} expYears={expYears} job={job} />
+        <img src={thumbnail || detailBgImg} css={s.imageStyle} alt="썸네일" />
         <ul css={s.hashTagListStyle}>
-          {detail.hashTagList.map((tag) => (
+          {hashTagList.map((tag) => (
             <li key={tag.id}>
               <TagChip showCloseButton={false} id={tag.id} content={tag.content} />
             </li>
           ))}
         </ul>
-        <div css={s.descriptionStyle}>{detail.content}</div>
+        <div css={s.descriptionStyle}>{content}</div>
       </div>
     </SideModal>
   );
--- src/pages/GroupChatListPage/components/GroupChatListAll/GroupChatListAll.tsx
+++ src/pages/GroupChatListPage/components/GroupChatListAll/GroupChatListAll.tsx
@@ -2,13 +2,23 @@ import { RotateLogoIcon } from "@/assets/svg";
 import { NoDataContainer } from "@/components";
 import { CHAT_ROOMS_DUMMY } from "@/constants/chatRoomsDummy";
 import GroupChatDetailModal from "@/pages/GroupChatListPage/components/GroupChatDetailModal/GroupChatDetailModal";
+import { GROUP_CHAT_DETAIL_DUMMY } from "@/pages/GroupChatListPage/constants/groupChatDetailDummy";
 import { useState } from "react";
 import * as s from "./GroupChatListAll.styles";
 
 const GroupChatListAll = () => {
   const items = CHAT_ROOMS_DUMMY;
+  const detail = GROUP_CHAT_DETAIL_DUMMY;
   const itemsCount = items.length;
-  const [isDetailModalVisible, setIsDetailModalVisible] = useState(true);
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+
+  const handleItemClick = (id: number) => {
+    setSelectedId(id);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedId(null);
+  };
 
   return (
     <div>
@@ -19,7 +29,11 @@ const GroupChatListAll = () => {
           <ul css={s.listContainerStyle(itemsCount)}>
             {items.map((chat) => (
               <li key={chat.id}>
-                <article css={s.listItemStyle}>
+                <article
+                  css={s.listItemStyle}
+                  onClick={() => handleItemClick(chat.id)}
+                  onKeyDown={() => handleItemClick(chat.id)}
+                >
                   <div css={s.infoLayoutStyle}>
                     <div css={s.textLayoutStyle}>
                       <RotateLogoIcon width={20} style={{ flexShrink: "0" }} />
@@ -36,7 +50,20 @@ const GroupChatListAll = () => {
               </li>
             ))}
           </ul>
-          <GroupChatDetailModal isVisible={isDetailModalVisible} setIsVisible={setIsDetailModalVisible} />
+          {selectedId !== null && (
+            <GroupChatDetailModal
+              id={detail.id}
+              title={detail.title}
+              thumbnail={detail.thumbnail}
+              content={detail.content}
+              hashTagList={detail.hashTagList}
+              nickname={detail.user.nickname}
+              expYears={detail.user.userIntro.expYears}
+              job={detail.user.userIntro.job}
+              isVisible={true}
+              setIsVisible={handleCloseModal}
+            />
+          )}
         </>
       )}
     </div>
