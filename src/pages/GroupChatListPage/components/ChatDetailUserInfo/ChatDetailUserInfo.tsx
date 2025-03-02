2da9f88496187a22782c382f7a7272b188c944d7
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-03-03 04:23:04 +0900

    #63 [FEAT] CardDetailUserInfo 컴포넌트 분리

---
+++ src/pages/GroupChatListPage/components/ChatDetailUserInfo/ChatDetailUserInfo.tsx
@@ -0,0 +1,29 @@
+import { UserBox } from "@/components";
+import * as s from "./ChatDetailUserInfo.styles";
+
+interface ChatDetailUserInfoProps {
+  nickname: string;
+  expYears: number;
+  job: string;
+}
+
+const ChatDetailUserInfo = ({ nickname, expYears, job }: ChatDetailUserInfoProps) => {
+  return (
+    <div css={s.userStyle}>
+      <UserBox name="이영희" />
+      <div css={{ display: "flex", flexDirection: "column", gap: "0.34rem" }}>
+        <div css={s.userInfoTopStyle}>
+          <h1>{nickname}</h1>
+          <div>방장</div>
+        </div>
+        <div css={s.userInfoBottomStyle}>
+          <p>{expYears}년차</p>
+          <div />
+          <p>{job}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatDetailUserInfo;
