d7c61ca768e086b2fa11987a82c1f521d2201ab7
namdaeun <nde40345@gmail.com>
2025-03-03 21:06:05 +0900

    #60 [FIX] 서버 선택 안함 버튼 제거

--- src/pages/SignupPage/components/InterestStep/InterestStep.tsx
+++ src/pages/SignupPage/components/InterestStep/InterestStep.tsx
@@ -9,7 +9,8 @@ interface InterestStepProps {
 }
 
 const InterestStep = ({ onPrev, onNext }: InterestStepProps) => {
-  const { interests, handleCheckboxChange, handleReset } = useInterestSelection();
+  const { interests, handleCheckboxChange, handleReset } =
+    useInterestSelection();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -19,14 +20,26 @@ const InterestStep = ({ onPrev, onNext }: InterestStepProps) => {
 
   return (
     <form onSubmit={handleSubmit} css={{ zIndex: 1 }}>
-      <AuthContainer title={TITLE.PROFILE} desc={DESC.INTEREST_INFO} size="large">
-        <Button variant="text" css={{ textDecoration: "none" }} onClick={handleReset}>
+      <AuthContainer
+        title={TITLE.PROFILE}
+        desc={DESC.INTEREST_INFO}
+        size="large"
+      >
+        <Button
+          variant="text"
+          css={{ textDecoration: "none" }}
+          onClick={handleReset}
+        >
           초기화
         </Button>
         <div css={s.boxWrapperStyle}>
           {INTERESTS.map((field) => (
             <label key={field} htmlFor={field} css={s.boxLayoutStyle}>
-              <CheckBox id={field} isChecked={interests.includes(field)} onChange={() => handleCheckboxChange(field)} />
+              <CheckBox
+                id={field}
+                isChecked={interests.includes(field)}
+                onChange={() => handleCheckboxChange(field)}
+              />
               <span css={s.labelStyle}>{field}</span>
             </label>
           ))}
@@ -34,9 +47,6 @@ const InterestStep = ({ onPrev, onNext }: InterestStepProps) => {
         <div css={{ display: "flex", gap: "2.6rem" }}>
           <Button onClick={onPrev} variant="tertiary">
             이전
-          </Button>
-          <Button variant="secondary" onClick={onNext}>
-            선택 안 함
           </Button>
           <Button type="submit" disabled={interests.length === 0}>
             완료
