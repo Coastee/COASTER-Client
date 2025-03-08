c907d89b2a4606672a3060adb7e526caffc1c5cc
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-03-08 15:23:35 +0900

    #69 [FEAT] sever 관련 api 파일 생성

---
+++ src/apis/server.ts
@@ -0,0 +1,24 @@
+// api.ts
+import { jsonKy } from "@/ky/ky";
+
+// [GET] Find All Servers
+export const fetchAllServers = async () => {
+  try {
+    const response = await jsonKy("/api/v1/servers").json();
+    console.log("fetchAllServers: ", response);
+    return response;
+  } catch (error) {
+    console.error("Error fetching all servers:", error);
+  }
+};
+
+// [GET] Server Home
+export const fetchServerHome = async (serverId: number) => {
+  try {
+    const response = await jsonKy(`/api/v1/servers/${serverId}`).json();
+    console.log("fetchServerHome: ", response);
+    return response;
+  } catch (error) {
+    console.error(`Error fetching home for server ${serverId}:`, error);
+  }
+};
