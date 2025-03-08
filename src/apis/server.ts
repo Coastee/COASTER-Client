5a3c0afa55f791cce4c8d46d2629a5d2977106b8
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-03-08 20:06:45 +0900

    #69 [FEAT] Enter, Exit Server api 코드 추가

--- src/apis/server.ts
+++ src/apis/server.ts
@@ -7,7 +7,7 @@ export const fetchAllServers = async () => {
     console.log("fetchAllServers: ", response);
     return response;
   } catch (error) {
-    console.error("Error fetching all servers:", error);
+    console.error("전체 서버 받아오기 실패:", error);
   }
 };
 
@@ -18,6 +18,29 @@ export const fetchServerHome = async (serverId: number) => {
     console.log("fetchServerHome: ", response);
     return response;
   } catch (error) {
-    console.error(`Error fetching home for server ${serverId}:`, error);
+    console.error(`홈 서버 정보 받아오기 실패 (${serverId}번 서버):`, error);
+  }
+};
+
+// [POST] Enter Server
+export const enterServer = async (serverId: number) => {
+  try {
+    const response = await jsonKy(`api/v1/servers/${serverId}`).json();
+    console.log("enterServer: ", response);
+    return response;
+  } catch (error) {
+    console.error(`${serverId}번 서버 입장 실패:`, error);
+  }
+};
+
+
+// [DELETE] Exit Server
+export const exitServer = async (serverId: number) => {
+  try {
+    const response = await jsonKy(`api/v1/${serverId}`).json();
+    console.log("exitServer: ", response);
+    return response;
+  } catch (error) {
+    console.error(`${serverId}번 서버 나가기 실패:`, error);
   }
 };
