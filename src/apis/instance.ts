ef739575a994ee758aad403f29ac005e6bae0ee1
namdaeun <nde40345@gmail.com>
2025-03-10 00:15:16 +0900

    #60 [FEAT] 인스턴스에 baseUrl 추가

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -2,6 +2,7 @@ import { getCookie } from "@/cookies/cookies";
 import ky from "ky";
 
 export const instance = ky.create({
+  prefixUrl: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -10,6 +11,7 @@ export const instance = ky.create({
 const accessToken = getCookie("accessToken");
 
 export const tokenInstance = ky.create({
+  prefixUrl: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${accessToken}`,
