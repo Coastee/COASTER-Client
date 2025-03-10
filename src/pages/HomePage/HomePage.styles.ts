e49d84b3fe348d8396838857ffd0b96e026c24d2
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-03-10 16:07:41 +0900

    #59 [FEAT] GlobalChatPage 구현 및 navigation 연결

--- src/pages/HomePage/HomePage.styles.ts
+++ src/pages/HomePage/HomePage.styles.ts
@@ -6,12 +6,13 @@ export const layoutStyle = css`
 
   gap: 4rem;
 
-  padding: 4.2rem 5rem 5rem 4.2rem;
+  padding: 4.2rem 5rem 0 4.2rem;
 
   width: calc(100vw - 32.6rem);
   height: 100vh;
 
-  overflow-x: scroll;
+  overflow: scroll;
+  ${scrollDarkStyle}
 `;
 
 export const leftLayoutStyle = css`
@@ -19,16 +20,4 @@ export const leftLayoutStyle = css`
   flex-direction: column;
 
   gap: 4rem;
-`;
-
-export const scrollLayoutStyle = css`
-  display: flex;
-  flex-direction: column;
-
-  gap: 4rem;
-
-  padding-right: 2rem;
-
-  overflow-y: scroll;
-  ${scrollDarkStyle}
 `;
