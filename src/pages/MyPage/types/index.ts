e453019a3fcbbe597655291dac5305e0abf51ed0
namdaeun <nde40345@gmail.com>
2025-03-13 00:00:29 +0900

    #65 [FEAT] api 리스폰스 인터페이스 추가

---
+++ src/pages/MyPage/types/index.ts
@@ -0,0 +1,39 @@
+interface UserIntro {
+  headline: string;
+  job: string;
+  expYears: number;
+}
+
+interface PageInfo {
+  lastPage: boolean;
+  totalPages: number;
+  totalElements: number;
+  size: number;
+}
+
+interface Experience {
+  id: number;
+  title: string;
+  contentList: string[];
+  startDate: [number, number, number, number, number, number, number];
+  endDate: [number, number, number, number, number, number, number];
+}
+
+interface ExperienceData {
+  pageInfo: PageInfo;
+  experienceList: Experience[];
+}
+
+export interface UserDetailTypes {
+  result: {
+    id: number;
+    profileImage: string;
+    nickname: string;
+    linkedInVerify: boolean;
+    userIntro: UserIntro;
+    bio: string;
+    urlList: string[];
+    dmRoomId: number;
+    experience: ExperienceData;
+  };
+}
