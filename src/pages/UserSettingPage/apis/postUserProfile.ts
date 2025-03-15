69298b684e4f768ce75ae8a7cd541acc15734d01
namdaeun <nde40345@gmail.com>
2025-03-15 16:40:32 +0900

    #81 [FEAT] data 다른 방식으로 넘김

--- src/pages/UserSettingPage/apis/postUserProfile.ts
+++ src/pages/UserSettingPage/apis/postUserProfile.ts
@@ -1,8 +1,12 @@
-import { tokenInstance } from "@/apis/instance";
+import { formDataInstance } from "@/apis/instance";
 import type { UserProfileTypes } from "@/pages/UserSettingPage/types/profile";
 
 export const postUserProfile = async (userId: number, data: UserProfileTypes) => {
-  const response = await tokenInstance.post(`api/v1/users/${userId}`, { json: data }).json();
+  const response = await formDataInstance
+    .post(`api/v1/users/${userId}`, {
+      json: { ...data },
+    })
+    .json();
 
   console.log(response);
 
