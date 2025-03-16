cb2e708653b55437743596adaa8c57e9c3309d1a
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-03-17 02:10:28 +0900

    #92 [FIX] dmSocketClient 함수 주석 구체화

--- src/sockets/dmSocketClient.ts
+++ src/sockets/dmSocketClient.ts
@@ -3,20 +3,21 @@ import { createClient } from "@/sockets/createClient";
 
 const accessToken = localStorage.getItem("accessToken");
 
-// DM 구독 및 메시지 전송을 위한 클라이언트 생성
+// 특정 DM 채팅방에 연결하여 실시간으로 메시지를 주고받는 STOMP 클라이언트 생성
 export const createStompClient = (roomId: number, onMessageReceived: (message: string) => void) => {
   const client = createClient((client) => {
-    console.log(`Connected to room ${roomId}`);
+    console.log(`Connected to DM room ${roomId}`);
 
     client.subscribe(`/sub/dms/${roomId}`, (message) => {
-      console.log("Received message: ", message.body);
+      console.log("Received DM message: ", message.body);
       onMessageReceived(message.body);
     });
   });
 
   client.activate();
 
-  // 메시지 전송 함수 반환
+  // 현재 DM 채팅방(roomId)에서 메시지를 전송하는 함수
+  // (클라이언트가 연결되지 않은 경우 전송하지 않음)
   const sendMessage = (messageBody: string) => {
     if (!client.connected) {
       console.error("STOMP Client is not connected");
@@ -32,12 +33,12 @@ export const createStompClient = (roomId: number, onMessageReceived: (message: s
   return { client, sendMessage };
 };
 
-// DM을 받은 후 채팅방 순서를 업데이트하는 함수
+// 채팅방 리스트 업데이트 된 걸 확인하기 위한 client (가장 최근 메시지가 상단에 올라올 수 있도록)
 export const createDMClient = (userId: number, onDMReceived: (dm: DMRoomTypes) => void) => {
   const client = createClient((client) => {
     client.subscribe(`/sub/users/dms/${userId}`, (message) => {
       console.log("New DM received:", message.body);
-      const dm = JSON.parse(message.body); // DM 메시지 파싱
+      const dm = JSON.parse(message.body);
       onDMReceived(dm);
     });
   });
@@ -47,7 +48,8 @@ export const createDMClient = (userId: number, onDMReceived: (dm: DMRoomTypes) =
   return client;
 };
 
-// 메시지 전송 함수 (DM 생성 및 DM 전송)
+// DM 채팅방에 입장하지 않고 1회성으로 메시지만 보낼 때 사용
+// 새로운 DM을 생성할 때(roomId: 존재) / 기존 DM방에 메시지를 보낼 때(roomId: null)
 export const sendMessage = (roomId: number | null, userId: number | null, messageBody: string) => {
   const destination = roomId ? `/pub/dms/${roomId}` : "/pub/dms/create";
   const body = roomId ? { content: messageBody } : { userId, content: messageBody };
