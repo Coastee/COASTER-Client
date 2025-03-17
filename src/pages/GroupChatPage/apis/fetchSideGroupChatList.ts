45635176439020318cbb0393d0773302b5276dfc
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-03-17 22:52:38 +0900

    #79 [RENAME] 사이드바 커피챗/그룹챗 리스트 파일 이름 변경(일반 리스트 조회와 구분)

--- src/pages/GroupChatPage/apis/fetchSideGroupChatList.ts
+++ src/pages/GroupChatPage/apis/fetchSideGroupChatList.ts
@@ -1,7 +1,7 @@
 import { tokenInstance } from "@/apis/instance";
 import type { ChatListTypes } from "@/pages/GroupChatPage/types";
 
-// 참여한 그룹챗 조회
+// 사이드 바: 참여한 그룹챗 리스트 조회
 export const fetchGroupChat = async (serverId: number) => {
   const response = await tokenInstance
     .get<ChatListTypes>(`api/v1/servers/${serverId}/groups?page=0&sort=name&scope=owner`)
