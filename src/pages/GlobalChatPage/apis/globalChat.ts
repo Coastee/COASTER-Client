0129f165791eb16e61b597144bd50a77061c88cb
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-03-18 16:21:21 +0900

    #92 [FIX] api에 get 메서드 명시적으로

--- src/pages/GlobalChatPage/apis/globalChat.ts
+++ src/pages/GlobalChatPage/apis/globalChat.ts
@@ -4,7 +4,9 @@ import type { ChatListResponseTypes } from "@/pages/GlobalChatPage/types/globalC
 // [GET] 전체 채팅 로그 조회
 export const fetchGlobalChatLogs = async (serverId: number): Promise<ChatListResponseTypes> => {
   try {
-    const response: ChatListResponseTypes = await tokenInstance(`api/v1/servers/${serverId}/groups/${serverId}`).json();
+    const response: ChatListResponseTypes = await tokenInstance
+      .get(`api/v1/servers/${serverId}/groups/${serverId}`)
+      .json();
     return response;
   } catch (error) {
     console.error("전체 채팅 로그 조회 실패:", error);
