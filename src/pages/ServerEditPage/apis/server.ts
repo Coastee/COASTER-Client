7c945f0ac965deae2c8d616db77647d4cec0253b
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-03-22 22:29:12 +0900

    #105 [FIX] 불필요한 try-catch문 삭제

--- src/pages/ServerEditPage/apis/server.ts
+++ src/pages/ServerEditPage/apis/server.ts
@@ -1,19 +1,9 @@
 import { tokenInstance } from "@/apis/instance";
 
 export const enterServer = async (serverId: number) => {
-  try {
-    const response = await tokenInstance.post(`api/v1/servers/${serverId}`).json();
-    return response;
-  } catch (error) {
-    console.error(`${serverId}번 서버 입장 실패:`, error);
-  }
+  return await tokenInstance.post(`api/v1/servers/${serverId}`).json();
 };
 
 export const exitServer = async (serverId: number) => {
-  try {
-    const response = await tokenInstance.delete(`api/v1/servers/${serverId}`).json();
-    return response;
-  } catch (error) {
-    console.error(`${serverId}번 서버 나가기 실패:`, error);
-  }
+  return await tokenInstance.delete(`api/v1/servers/${serverId}`).json();
 };
