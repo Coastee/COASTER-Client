9df32dc628b391252352eb41775b7f1ac8920f40
namdaeun <nde40345@gmail.com>
2025-03-24 21:07:22 +0900

    #123 [CHORE] 인스턴스 변경

--- src/pages/UserSettingPage/apis/updateExperience.ts
+++ src/pages/UserSettingPage/apis/updateExperience.ts
@@ -1,8 +1,8 @@
-import { tokenInstance } from "@/apis/instance";
+import { formDataInstance } from "@/apis/instance";
 import type { ExperienceTypes } from "@/pages/UserSettingPage/types/career";
 
 export const updateExperience = async (userId: number, experienceId: number, data: ExperienceTypes) => {
-  const response = await tokenInstance
+  const response = await formDataInstance
     .post(`api/v1/users/${userId}/experiences/${experienceId}`, { json: data })
     .json();
 
