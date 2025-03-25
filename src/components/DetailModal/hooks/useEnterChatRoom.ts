4ba2010998109c6af531d9dfdfdd273a0a6a4c28
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-03-26 02:03:52 +0900

    #133 [FEAT] 티타임 생성, 입장, 퇴장 시 스케줄 바로 업데이트

--- src/components/DetailModal/hooks/useEnterChatRoom.ts
+++ src/components/DetailModal/hooks/useEnterChatRoom.ts
@@ -1,9 +1,16 @@
 import { enterChatRoom } from "@/components/DetailModal/apis/enterChatRoom";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useEnterChatRoom = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: ({ serverId, chatRoomType, groupId }: { serverId: number; chatRoomType: string; groupId: number }) =>
       enterChatRoom(serverId, chatRoomType, groupId),
+    onSuccess: (_, { chatRoomType }) => {
+      if (chatRoomType === "meetings") {
+        queryClient.invalidateQueries({ queryKey: ["schedule"] });
+      }
+    },
   });
 };
