db7a86487945a5691be43874c6316d5a1ff89933
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-03-28 00:10:47 +0900

    #146 [FIX] DMList에서 선택한 사용자에 대해 ChatRoom 상단에 프로필 이미지 잘 뜨도록

--- src/pages/DMPage/components/ChatRoom/ChatRoom.tsx
+++ src/pages/DMPage/components/ChatRoom/ChatRoom.tsx
@@ -82,7 +82,7 @@ const ChatRoom = ({ dmList, userId, roomId, setRoomId, nickname, expYears, job, 
     <section css={s.wrapperStyle}>
       <header css={s.headerStyle}>
         <div css={s.titleLayoutStyle}>
-          <UserBox name={userInfo?.nickname || nickname || "사용자 없음"} profileImage={profileImage} />
+          <UserBox name={userInfo?.nickname || nickname || "사용자 없음"} profileImage={userInfo?.profileImage || profileImage} />
           <div css={s.infoLayoutStyle}>
             <h1>{userInfo?.nickname || nickname || "사용자 없음"}</h1>
             <div css={{ display: "flex", gap: "0.8rem", alignItems: "center" }}>
