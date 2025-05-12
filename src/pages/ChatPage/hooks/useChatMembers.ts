6819db967144ca8a243d1e832bee8ebd449de6f9
namdaeun <nde40345@gmail.com>
2025-05-13 00:59:41 +0900

    #154 [REFACTOR] type 분리

--- src/pages/ChatPage/hooks/useChatMembers.ts
+++ src/pages/ChatPage/hooks/useChatMembers.ts
@@ -1,11 +1,19 @@
 import { fetchChatMembers } from "@/pages/ChatPage/apis/fetchChatMembers";
+import type { ChatRoom } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
 // 채팅 멤버 조회 API 훅
-export const useFetchChatMembers = (serverId: number | null, type: "groups" | "meetings", roomId: number) => {
+export const useFetchChatMembers = (
+  serverId: number | null,
+  type: ChatRoom,
+  roomId: number
+) => {
   return useQuery({
     queryKey: ["chatMembers", serverId, roomId],
-    queryFn: () => (serverId !== null ? fetchChatMembers(serverId, type, roomId) : Promise.reject("serverId is null")),
+    queryFn: () =>
+      serverId !== null
+        ? fetchChatMembers(serverId, type, roomId)
+        : Promise.reject("serverId is null"),
     enabled: !!serverId,
   });
 };
